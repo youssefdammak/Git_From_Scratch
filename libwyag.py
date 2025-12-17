c0bed559ac96c217889acf0fae0ab116c7a0e0bb
youssef dammak <youssedbenmaherdammak@gmail.com>
2025-12-16 20:27:29 -0500

    Added git tree object

--- libwyag.py
+++ libwyag.py
@@ -519,6 +519,105 @@ def log_graphviz(repo, sha, seen):
         print (f"  c_{sha} -> c_{p};")
         log_graphviz(repo, p, seen)
         
+"""
+A Git commit is an immutable object whose SHA-1 hash is computed from its
+entire contents: the filesystem snapshot (tree), parent commit(s), author
+and committer metadata, timestamps, message, and optional signature.
+Because each commit references its parent(s), it is cryptographically
+linked to the full history of the repository and can never be modified
+without creating a new commit.
+"""
+
+class GitTreeLeaf (object):
+    def __init__(self, mode, path, sha):
+        self.mode = mode # A file mode in a Git tree is a numeric code that tells Git what kind of filesystem object it is and what permissions it has.
+        self.path = path
+        self.sha = sha
+
+""""
+A tree is the concatenation of records of the format:
+[mode] space [path] 0x00 [sha-1]
+
+A tree object is just a sequence of entries, all stored one after another:
+
+[mode] [space] [path] [null byte] [20-byte SHA]
+[mode] [space] [path] [null byte] [20-byte SHA]
+[mode] [space] [path] [null byte] [20-byte SHA]
+...
+
+So the same structure repeats over and over.
+Each record = one file or directory entry
+"""
+
+def tree_parse_one(raw, start=0):
+    # Find the space terminator of the mode
+    x = raw.find(b' ', start)
+    assert x-start == 5 or x-start==6
+
+    # Read the mode
+    mode = raw[start:x]
+    if len(mode) == 5:
+        # Normalize to six bytes.
+        mode = b"0" + mode
+
+    # Find the NULL terminator of the path
+    y = raw.find(b'\x00', x)
+    # and read the path
+    path = raw[x+1:y]
+
+    # Read the SHA…
+    raw_sha = int.from_bytes(raw[y+1:y+21], "big")
+    # and convert it into an hex string, padded to 40 chars
+    # with zeros if needed.
+    sha = format(raw_sha, "040x")
+    return y+21, GitTreeLeaf(mode, path.decode("utf8"), sha)
+
+def tree_parse(raw):
+    pos = 0
+    max = len(raw)
+    ret = list()
+    while pos < max:
+        pos, data = tree_parse_one(raw, pos)
+        ret.append(data)
+
+    return ret
+
+# Notice this isn't a comparison function, but a conversion function.
+# Python's default sort doesn't accept a custom comparison function,
+# like in most languages, but a `key` arguments that returns a new
+# value, which is compared using the default rules.  So we just return
+# the leaf name, with an extra / if it's a directory.
+def tree_leaf_sort_key(leaf):
+    if leaf.mode.startswith(b"10"):
+        return leaf.path
+    else:
+        return leaf.path + "/"
+
+def tree_serialize(obj):
+    obj.items.sort(key=tree_leaf_sort_key)
+    ret = b''
+    for i in obj.items:
+        ret += i.mode
+        ret += b' '
+        ret += i.path.encode("utf8")
+        ret += b'\x00'
+        sha = int(i.sha, 16)
+        ret += sha.to_bytes(20, byteorder="big")
+    return ret
+
+class GitTree(GitObject):
+    fmt=b'tree'
+
+    def deserialize(self, data):
+        self.items = tree_parse(data)
+
+    def serialize(self):
+        return tree_serialize(self)
+
+    def init(self):
+        self.items = list()
+
+
 def main(argv=sys.argv[1:]):
     args = argparser.parse_args(argv)
     match args.command:
